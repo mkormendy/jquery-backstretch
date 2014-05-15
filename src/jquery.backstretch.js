ccdb7f97cd65ac94d388374d566766fd70b07267
Fokke Zandbergen <mail@fokkezb.nl>
2014-05-15 08:43:35 +0200

    Fixes orientation bug on mobile Updates jquery.backstretch.js

--- src/jquery.backstretch.js
+++ src/jquery.backstretch.js
@@ -215,7 +215,8 @@
       resize: function () {
         try {
           var bgCSS = {left: 0, top: 0}
-            , rootWidth = this.isBody ? this.$root.width() : this.$root.innerWidth()
+            , landscape = (Math.ceil(window.innerWidth / window.innerHeight) > Math.ceil(screen.width / screen.height))
+            , rootWidth = this.isBody ? (isMobile ? (landscape ? screen.height : screen.width) : this.$root.width()) : this.$root.innerWidth()
             , bgWidth = rootWidth
             , rootHeight = this.isBody ? ( window.innerHeight ? window.innerHeight : this.$root.height() ) : this.$root.innerHeight()
             , bgHeight = bgWidth / this.$img.data('ratio')
